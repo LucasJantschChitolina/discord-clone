5e0a435735307ca6a65262b4d33ec0004e586fe2
Chitolina <lucas.chitolina01@gmail.com>
2024-04-07 18:34:14 -0300

    feat: adding image upload to project

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,7 +24,8 @@ import {
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
 	name: z.string().min(1, {
@@ -76,7 +77,20 @@ export function InitialModal() {
 					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 						<div className="space-y-8 px-6">
 							<div className="flex items-center justify-center text-center">
-								TODO: IMAGE UPLOAD
+								<FormField
+									control={form.control}
+									name="imageUrl"
+									render={({ field }) => (
+										<FormItem>
+											<FormControl>
+												<FileUpload
+													endpoint="serverImage"
+													value={field.value}
+													onChange={field.onChange}
+												/>
+											</FormControl>
+										</FormItem>
+									)} />
 							</div>
 
 							<FormField
